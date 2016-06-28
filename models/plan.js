baaa1c33c04503e5f31506c9b21c1330f60635a1
Greg Magdsick <greg.magdsick@gmail.com>
2016-06-28 10:20:58 -0700

    updated plan model

--- models/plan.js
+++ models/plan.js
@@ -1,9 +1,49 @@
 const mongoose = require('mongoose');
 
-var householdMemberSchema = new mongoose.Schema({
-  relationship: String,
-  firstName: String,
-  lastName: String,
+var addressSchema = new mongoose.Schema({
+  streetAddress: String,
+  streetAddressPt2: String,
+  City: String,
+  State: String,
+  Zip: Number
+});
+
+var baseKitSchema = new mongoose.Schema({
+  water: { type: Boolean, required: true },
+  food: { type: Boolean, required: true },
+  noaaWeatherRadio: { type: Boolean, required: true },
+  flashlight: { type: Boolean, required: true },
+  extraBatteries: { type: Boolean, required: true },
+  firstAidKit: { type: Boolean, required: true },
+  whistle: { type: Boolean, required: true },
+  dustMask: { type: Boolean, required: true },
+  sheetingAndDuctTape: { type: Boolean, required: true },
+  moistTowelettes: { type: Boolean, required: true },
+  garbageBagsAndPlasticTies: { type: Boolean, required: true },
+  wrenchOrPliers: { type: Boolean, required: true },
+  canOpener: { type: Boolean, required: true },
+  localMaps: { type: Boolean, required: true }
+});
+
+var iceSchema = new mongoose.Schema({
+  firstName: { contentType: String, required: true },
+  lastName: { contentType: String, required: true },
+  email: String,
+  primaryPhone: { contentType: String, required: true },
+  secondaryPhone: String,
+  address: addressSchema
+});
+
+var neighborhoodSchema = new mongoose.Schema({
+  zip: { contentType: Number, required: true },
+  name: { contentType: String, required: true },
+  neighborhoodRally: [addressSchema]
+});
+
+var personSchema = new mongoose.Schema({
+  relationship: { contentType: String, required: true },
+  firstName: { contentType: String, required: true },
+  lastName: { contentType: String, required: true },
   email: String,
   primaryPhone: String,
   secondaryPhone: String,
@@ -11,70 +51,28 @@ var householdMemberSchema = new mongoose.Schema({
   weight: String,
   picture: { data: Buffer, contentType: String },
   workSchoolName: String,
-  streetAddress: String,
-  streetAddressPt2: String,
-  city: String,
-  state: String,
-  zip: Number,
-  birthYear: Number
-});
-
-var iceSchema = new mongoose.Schema({
-  firstName: String,
-  lastName: String,
-  email: String,
-  primaryPhone: String,
-  secondaryPhone: String,
-  streetAddress: String,
-  streetAddressPt2: String,
-  city: String,
-  state: String,
-  zip: Number
+  workSchoolAddress: addressSchema,
+  birthYear: Number,
+  workSchoolRally: addressSchema
 });
 
 var planSchema = new mongoose.Schema({
-    firstName: { type: String, required: true },
-    lastName: { type: String, required: true },
-    email: { type: String, required: true, unique: true },
-    memberID: String,
-    primaryPhone: String,
-    secondaryPhone: String,
-    streetAddress: String,
-    streetAddressPt2: String,
-    city: String,
-    state: String,
-    zip: Number,
+    leader: personSchema,
+    neighborhood: neighborhoodSchema,
     reminderDate: { type: Date, default: Date() },
     reminderFrequency: { type: Number, default: 91 },
-    householdMembers: [householdMemberSchema],
+    householdMembers: [personSchema],
     ice: [iceSchema],
     rallyPoints: {
       immediate: String,
-      rallyStreetAddress: String,
-      rallyStreetAddressPt2: String,
-      rallyCity: String,
-      rallyState: String,
-      rallyZip: Number
+      rallyAddress: addressSchema
     },
     emergencykit: {
       location: String,
-      water: { type: Boolean, required: true },
-      food: { type: Boolean, required: true },
-      noaaWeatherRadio: { type: Boolean, required: true },
-      flashlight: { type: Boolean, required: true },
-      extraBatteries: { type: Boolean, required: true },
-      firstAidKit: { type: Boolean, required: true },
-      whistle: { type: Boolean, required: true },
-      dustMask: { type: Boolean, required: true },
-      sheetingAndDuctTape: { type: Boolean, required: true },
-      moistTowelettes: { type: Boolean, required: true },
-      garbageBagsAndPlasticTies: { type: Boolean, required: true },
-      wrenchOrPliers: { type: Boolean, required: true },
-      canOpener: { type: Boolean, required: true },
-      localMaps: { type: Boolean, required: true },
+      baseKit: baseKitSchema,
+      addOns: {},
       kitReminderDate: Number
     }
   });
-
 
   module.exports = exports = mongoose.model('Plan', planSchema);
