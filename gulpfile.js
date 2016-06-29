4c2a62e5734669644a5057d7009e73535976feab
Greg Magdsick <greg.magdsick@gmail.com>
2016-06-29 10:41:16 -0700

    mas scaffold

--- gulpfile.js
+++ gulpfile.js
@@ -4,11 +4,17 @@ const gulp = require('gulp');
 const eslint = require('gulp-eslint');
 const mocha = require('gulp-mocha');
 const webpack = require('webpack-stream');
+const maps = require('gulp-sourcemaps');
+const sass = require('gulp-sass');
 
 const serverScripts = ['index.js', 'lib/*.js', 'test/*.js', 'models/*.js', 'routes/*.js'];
 const appScripts = ['./app/**/*.jsx'];
 
-gulp.task('webpack:dev', () => {
+gulp.task('watch', () => {
+  gulp.watch(serverScripts, ['lint', 'test']);
+});
+
+gulp.task('webpack:dev', ['scss:dev', 'html:dev', 'img:dev'], () => {
   return gulp.src('app/js/entry.jsx')
     .pipe(webpack({
       output: {
@@ -27,6 +33,24 @@ gulp.task('webpack:dev', () => {
     .pipe(gulp.dest('./build'));
 });
 
+gulp.task('scss:dev', () => {
+  return gulp.src('app/scss/main.scss')
+    .pipe(maps.init())
+    .pipe(sass().on('error', sass.logError))
+    .pipe(maps.write('./'))
+    .pipe(gulp.dest('./build'));
+});
+
+gulp.task('html:dev', () => {
+  return gulp.src('app/index.html')
+    .pipe(gulp.dest('./build'));
+});
+
+gulp.task('img:dev', () => {
+  return gulp.src('app/img/*')
+    .pipe(gulp.dest('./build/img'));
+});
+
 gulp.task('lint', () => {
   return gulp.src(serverScripts)
   .pipe(eslint())
@@ -36,10 +60,6 @@ gulp.task('lint', () => {
 gulp.task('test', () => {
   return gulp.src('test/*test.js')
   .pipe(mocha());
-});
-
-gulp.task('watch', () => {
-  gulp.watch(serverScripts, ['lint', 'test']);
 });
 
 gulp.task('default', ['lint', 'test']);
