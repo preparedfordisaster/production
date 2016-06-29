e60c44b7b2c2f95dff46781971ed91d11c641d58
Greg Magdsick <greg.magdsick@gmail.com>
2016-06-29 12:38:52 -0700

    first try

--- app/js/entry.jsx
+++ app/js/entry.jsx
@@ -6,19 +6,55 @@ const ReactRouter = require('react-router');
 var Landing = new React.createClass({
   render: function() {
     return (
-      <article>
+      <section>
         <button>LOGIN</button>
         <button>REGISTER</button>
-      </article>
-    )
+      </section>
+    );
+  }
+});
+
+var Login = new React.createClass({
+  render: function() {
+    return (
+      <form>
+        <label>EMAIL</label>
+        <input type="text" name="email"></input>
+        <label>PASSWORD</label>
+        <input type="password" name="password"></input>
+        <button>LOGIN</button>
+      </form>
+    );
+  }
+});
+
+var Register = new React.createClass({
+  render: function() {
+    return (
+      <form>
+        <label>FIRST NAME</label>
+        <input type="text" name="firstName"></input>
+        <label>LAST NAME</label>
+        <input type="text" name="lastName"></input>
+        <label>EMAIL</label>
+        <input type="email" name="email"></input>
+        <label>CONFIRM EMAIL</label>
+        <input type="email" name="confirmEmail"></input>
+        <label>PASSWORD</label>
+        <input type="password" name="password"></input>
+        <label>CONFIRM PASSWORD</label>
+        <input type="password" name="confirmPassword"></input>
+        <button>REGISTER</button>
+      </form>
+    );
   }
 });
 
 ReactDOM.render(
   <ReactRouter.Router>
-    <ReactRouter.Route path='/' component='Landing'>
-
-    </ReactRouter.Route>
+    <ReactRouter.Route path='/' component={Landing}></ReactRouter.Route>
+    <ReactRouter.Route path='/login' component={Login}></ReactRouter.Route>
+    <ReactRouter.Route path='/register' component={Register}></ReactRouter.Route>
   </ReactRouter.Router>,
-  destination
+  document.querySelector('.app')
 );
