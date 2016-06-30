e4debd06e56ddfba03ee485a51325f7b57edd3a8
bharding2 <b.harding@gmail.com>
2016-06-30 16:18:40 -0700

    cleanup

--- routes/auth_routes.js
+++ routes/auth_routes.js
@@ -11,64 +11,24 @@ passport.use(new LocalStrategy(User.authenticate()));
 var router = module.exports = exports = express.Router();
 
 router.post('/signup', bodyParser, (req, res) => {
-  User.register(new User({ username: req.body.username }), req.body.password, (err, user) => {
+  var password = req.body.password;
+  req.body.password = null;
+
+  if (!password) return res.status(500).json({ msg: 'no blank passwords' });
+
+  User.register(new User({ username: req.body.username }), password, (err, user) => {
+    password = null;
+
     if (err) return console.log(err);
     res.status(200).json({
-      msg: 'signup testing',
       token: jwt.sign({ idd: user.hash }, process.env.APP_SECRET)
     });
   });
-
-  // var password = req.body.password;
-  // req.body.password = null;
-  //
-  // if (!password) return res.status(500).json({ msg: 'Password is needed' });
-  //
-  // var newUser = new User(req.body);
-  // newUser.generateHash(password);
-  // password = null;
-  //
-  // newUser.save((err, user) => {
-  //   if (err) return res.status(500).json({ msg: 'Could not create user' });
-  //
-  //   user.generateToken((err, token) => {
-  //     if (err) return res.status(500).json({ msg: 'Could not generate token' });
-  //
-  //     res.json({ token });
-  //   });
-  // });
 });
 
 router.get('/signin', basicHTTP, passport.authenticate('local', { session: false }),
 (req, res) => {
   res.status(200).json({
-    msg: 'login testing',
     token: jwt.sign({ idd: req.user.hash }, process.env.APP_SECRET)
   });
-
-  // jwt signing using hash instead of find hash
-
-  // User.findOne({ username: req.auth.username }, (err, user) => {
-  //   if (err) {
-  //     return res.status(500).json({
-  //        msg: 'Could not authenticate. Please re-enter username and password again'
-  //      });
-  //   }
-  //   if (!user) {
-  //     return res.status(500).json({
-  //        msg: 'Could not authenticate. Please re-enter username and password again'
-  //      });
-  //   }
-  //   if (!user.compareHash(req.auth.password)) {
-  //     return res.status(500).json({
-  //        msg: 'Could not authenticate. Please re-enter username and password again'
-  //      });
-  //   }
-  //   user.generateToken((err, token) => {
-  //     if (err) {
-  //       return res.status(500).json({ msg: 'Could not generate token. Please try again later' });
-  //     }
-  //     res.json({ token });
-  //   });
-  // });
 });
