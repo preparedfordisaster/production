8b756cddaa015cb5beebd602d9489e3684714cfe
bharding2 <b.harding@gmail.com>
2016-07-22 00:48:02 -0700

    noodling with redux

--- app/js/lib/login.jsx
+++ app/js/lib/login.jsx
@@ -1,4 +1,35 @@
 const React = require('react');
+const Redux = require('redux');
+const ReactRedux = require('react-redux');
+const connect = ReactRedux.connect;
+
+const userReducer = (state, action) => {
+  var newState = Object.assign({}, state, { username: 'ben' });
+  return newState;
+};
+
+const planReducer = (state, action) => {
+  var newState = Object.assign({}, state, { plan: 'ben plan' });
+  return newState;
+};
+
+const reducers = Redux.combineReducers({
+  userState: userReducer,
+  planState: planReducer
+});
+
+let store = Redux.createStore(reducers);
+
+store.subscribe(() => {
+  console.log(store.getState());
+});
+
+const mapStateToProps = (store) => {
+  return {
+    username: store.userState.username,
+    plan: store.planState.plan
+  };
+};
 
 const Login = new React.createClass({
   render: function() {
@@ -14,4 +45,4 @@ const Login = new React.createClass({
   }
 });
 
-module.exports = Login;
+module.exports = connect(mapStateToProps)(Login);
